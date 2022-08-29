503c29136f73afc34744503670989c21fd5827fe
zeredogo <zeredogo@gmail.com>
2022-08-29 01:59:25 +0100

    updating commit

--- src/App.js
+++ src/App.js
@@ -32,7 +32,7 @@ const App = () => {
             type='text'
             value={color}
             onChange={(e) => setColor(e.target.value)}
-            placeholder='#0000'
+            placeholder='#0000ff'
             className={`${error ? 'error' : null}`}
           />
           <button className='btn' type='submit'>
